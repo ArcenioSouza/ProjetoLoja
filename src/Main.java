624a816cefaf0afa7f134f8c9e75f257e8c3916a
Arcenio Souza <arcenio_neto@icloud.com>
2022-11-07 14:14:42 -0300

    feat: :sparkles: Adiciona orçamento extra com padrão state

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,8 @@ import java.math.BigDecimal;
 
 public class Main {
     public static void main(String[] args) {
-        Orcamento orcamento1 = new Orcamento(new BigDecimal("100"), 6);
-        Orcamento orcamento2 = new Orcamento(new BigDecimal("1000"), 1);
+        Orcamento orcamento1 = new Orcamento(new BigDecimal("100"), 6, situacao);
+        Orcamento orcamento2 = new Orcamento(new BigDecimal("1000"), 1, situacao);
 
         CalculadoraDeImposto calculadora = new CalculadoraDeImposto();
         CalculadoraDeDesconto calculadoraDeDesconto = new CalculadoraDeDesconto();
--- src/br/com/loja/orcamento/Orcamento.java
+++ src/br/com/loja/orcamento/Orcamento.java
@@ -1,15 +1,45 @@
 package br.com.loja.orcamento;
 
+import br.com.loja.orcamento.situacao.EmAnalise;
+import br.com.loja.orcamento.situacao.SituacaoOrcamento;
+
 import java.math.BigDecimal;
 
 public class Orcamento {
     private BigDecimal valor;
     private int quantidadeItens;
+    private SituacaoOrcamento situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+        this.situacao = new EmAnalise();
     }
+    public void aplicarDescontoExtra(){
+        BigDecimal valorDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao.aprovar(this);
+    }
+
+    public void reprovar(){
+        this.situacao.reprovar(this);
+    }
+
+    public void finalizar(){
+        this.situacao.finalizar(this);
+    }
+
+    public void setSituacao(SituacaoOrcamento situacao) {
+        this.situacao = situacao;
+    }
+
+    public SituacaoOrcamento getSituacao() {
+        return situacao;
+    }
+
 
     public int getQuantidadeItens() {
         return quantidadeItens;
